88d08e3274fe3e99a29d771d4632c2d4bfa4fec3
megha-panasala <143687239+megha-panasala@users.noreply.github.com>
2024-07-30 16:05:43 +0530

    Add files via upload

---
+++ game1.js
@@ -0,0 +1,8 @@
+let num = 25;
+let usernum = prompt("Guess the number");
+
+while (usernum != num) {
+    usernum = prompt("u guess wrong number guess again")
+}
+
+console.log("You guess right number")
